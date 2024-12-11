dfa1fc3f201564b04613804cefbbe6cd028d2de9
Mohamad Rizki <mohamad.rizki.dafianto@gmail.com>
2024-12-11 14:45:05 +0700

    init: initial commit

---
+++ src/main/java/com/example/aesdemo/SpringAesDemoApplication.java
@@ -0,0 +1,13 @@
+package com.example.aesdemo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringAesDemoApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringAesDemoApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/example/aesdemo/SpringAesDemoApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.aesdemo;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringAesDemoApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
